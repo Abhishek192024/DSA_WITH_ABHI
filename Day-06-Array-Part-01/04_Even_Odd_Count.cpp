88c81e318b9fa26c3befef76fa36aac424f537c0
Abhishek Chaurasiya <chaurasiyaabhi684@gmail.com>
2026-09-12 15:28:08 +0000

    Add C++ program to count even and odd elements in an array

--- Day-06-Array-Part-01/04_Even_Odd_Count.cpp
+++ Day-06-Array-Part-01/04_Even_Odd_Count.cpp
@@ -11,3 +11,32 @@
 // Hint: Check each element using % 2.
 // Time: O(n), Space: O(n)
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i<= n-1; i++){
+        cin >> arr[i];
+    }
+
+    int evenCount = 0;
+    int oddCount = 0;
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] % 2 == 0){
+            evenCount++;
+        }
+        else{
+            oddCount++;
+        }
+    }
+
+    cout << "Even = " << evenCount << endl;
+    cout << "Odd = " << oddCount << endl;
+
+    return 0;
+}
