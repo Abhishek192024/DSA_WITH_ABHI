ff9dd0acec27cc93a1dd0799ac5f8e441162b3bb
Abhishek Chaurasiya <chaurasiyaabhi684@gmail.com>
2026-09-12 15:05:24 +0000

    Add C++ program to find maximum and minimum elements in an array

--- Day-06-Array-Part-01/03_Max_Min.cpp
+++ Day-06-Array-Part-01/03_Max_Min.cpp
@@ -11,3 +11,32 @@
 // Hint: Initialize max and min with arr[0].
 // Time: O(n), Space: O(n)
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i<= n-1; i++){
+        cin >> arr[i];
+    }
+
+    int maxi = arr[0];
+    int mini = arr[0];
+
+    for(int i = 1; i< n; i++){
+
+        if (arr[i] > maxi)
+        maxi = arr[i];
+
+        if (arr[i] < mini)
+        mini = arr[i];
+    }
+
+    cout << "Maximum = " << maxi << endl;
+    cout << "Manimum = " << mini << endl;
+
+    return 0;
+}
