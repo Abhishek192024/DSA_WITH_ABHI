d3382ca9494730a0ae35a71bb8886bbdc6be5fe1
Abhishek Chaurasiya <chaurasiyaabhi684@gmail.com>
2026-09-13 05:02:21 +0000

    Add C++ program to reverse an array in-place

--- Day-06-Array-Part-01/06_Reverse_Array.cpp
+++ Day-06-Array-Part-01/06_Reverse_Array.cpp
@@ -8,4 +8,27 @@
 //
 // Hint: Use two indices: start and end.
 // Time: O(n), Space: O(1) extra
+#include<iostream>
+using namespace std;
 
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    int start = 0, end = n-1;
+    while(start < end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    for (int i = 0; i < n; i++){
+        cout << arr[i] <<"";
+    }
+    return 0;
+    
+}
