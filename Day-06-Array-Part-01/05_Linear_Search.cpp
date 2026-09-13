f6adae181f77490de8f4641aa80eddc5ef18717a
Abhishek Chaurasiya <chaurasiyaabhi684@gmail.com>
2026-09-13 03:44:29 +0000

    Add C++ program for linear search in an array

--- Day-06-Array-Part-01/05_Linear_Search.cpp
+++ Day-06-Array-Part-01/05_Linear_Search.cpp
@@ -11,3 +11,32 @@
 // Hint: Traverse from left to right and stop at the first match.
 // Time: O(n), Space: O(n)
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter Your Array Number" <<endl;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int key ;
+    cout << "Enter Your Key" << endl;;
+    cin >> key;
+
+    int index = -1;
+
+    for (int i = 0; i < n; i++){
+        if(arr[i] == key){
+            index = i;
+            break;
+        }
+    }
+    cout << index;
+    
+    return 0;
+}
